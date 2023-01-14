c954a7d86651eb49b2ffd5deb7e7e6b5549d555f
bhendi-boi <bhendiisgreen@gmail.com>
2023-01-14 14:16:58 +0530

    it's a pwa now

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,50 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"./",
-  plugins: [react()],
-})
+	base: "./",
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: "prompt",
+			includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+			manifest: {
+				name: "Weather Ups",
+				short_name: "Weathe Ups",
+				description: "An app that can show weather forecast for your city.",
+				icons: [
+					{
+						src: "/android-chrome-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "/android-chrome-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+					{
+						src: "/apple-touch-icon.png",
+						sizes: "180x180",
+						type: "image/png",
+						purpose: "apple touch icon",
+					},
+					{
+						src: "/maskable_icon.png",
+						sizes: "225x225",
+						type: "image/png",
+						purpose: "any maskable",
+					},
+				],
+				theme_color: "#171717",
+				background_color: "#e8ebf2",
+				display: "standalone",
+				scope: "/",
+				start_url: "/",
+				orientation: "portrait",
+			},
+		}),
+	],
+});
