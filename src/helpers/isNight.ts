94e8b65e97037cf68b73b187a2c8347e4263d813
bhendi-boi <bhendiisgreen@gmail.com>
2023-02-01 23:04:28 +0530

    🪛 no crashes now

--- src/helpers/isNight.ts
+++ src/helpers/isNight.ts
@@ -1,5 +1,7 @@
+import useWeather from "../hooks/useWeather";
 export function isNight(){
-    const sunset = new Date(JSON.parse(localStorage.getItem("APIRES") as string)["daily"]["sunset"][0] * 1000) ;
+    const [apiRes] = useWeather()
+    const sunset = new Date(apiRes["daily"]["sunset"][0] * 1000) ;
         const sunsetHours = sunset.getHours()
     const sunsetMin = sunset.getMinutes()
     const currentTime = new Date()
