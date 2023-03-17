504f73b8540de69c34671a88d3dd1ad58144352d
bhendi-boi <bhendiisgreen@gmail.com>
2023-03-17 11:56:27 +0530

    refactoring in viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,50 +1,48 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 
+const manifestForPlugin: Partial<VitePWAOptions> = {
+	registerType: "prompt",
+	includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+	manifest: {
+		name: "Weather Ups",
+		short_name: "Weathe Ups",
+		description: "An app that can show weather forecast for your city.",
+		icons: [
+			{
+				src: "/android-chrome-192x192.png",
+				sizes: "192x192",
+				type: "image/png",
+			},
+			{
+				src: "/android-chrome-512x512.png",
+				sizes: "512x512",
+				type: "image/png",
+			},
+			{
+				src: "/apple-touch-icon.png",
+				sizes: "180x180",
+				type: "image/png",
+				purpose: "apple touch icon",
+			},
+			{
+				src: "/maskable_icon.png",
+				sizes: "225x225",
+				type: "image/png",
+				purpose: "any maskable",
+			},
+		],
+		theme_color: "#171717",
+		background_color: "#e8ebf2",
+		display: "standalone",
+		scope: "/",
+		start_url: "/",
+		orientation: "portrait",
+	},
+};
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: "./",
-	plugins: [
-		react(),
-		VitePWA({
-			registerType: "prompt",
-			includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
-			manifest: {
-				name: "Weather Ups",
-				short_name: "Weathe Ups",
-				description: "An app that can show weather forecast for your city.",
-				icons: [
-					{
-						src: "/android-chrome-192x192.png",
-						sizes: "192x192",
-						type: "image/png",
-					},
-					{
-						src: "/android-chrome-512x512.png",
-						sizes: "512x512",
-						type: "image/png",
-					},
-					{
-						src: "/apple-touch-icon.png",
-						sizes: "180x180",
-						type: "image/png",
-						purpose: "apple touch icon",
-					},
-					{
-						src: "/maskable_icon.png",
-						sizes: "225x225",
-						type: "image/png",
-						purpose: "any maskable",
-					},
-				],
-				theme_color: "#171717",
-				background_color: "#e8ebf2",
-				display: "standalone",
-				scope: "/",
-				start_url: "/",
-				orientation: "portrait",
-			},
-		}),
-	],
+	plugins: [react(), VitePWA(manifestForPlugin)],
 });
