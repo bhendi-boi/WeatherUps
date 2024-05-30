9be4e4d1d7ccf364fd8fe332017393671548c3ab
bhendi-boi <bhendiisgreen@gmail.com>
2024-05-30 14:50:33 +0530

    removed section titles as they are redundant

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,32 +1,26 @@
-import { useState, useEffect } from "react";
+import { useState,useEffect } from "react";
 import { HourlyWeatherCard } from "./WeatherCard";
 import getHourlyData from "../helpers/getHourlyData";
 import { HourlyData } from "../helpers/getHourlyData";
-import useWeather from "../hooks/useWeather";
 import useNight from "../hooks/useNight";
 
 const Carousel = (apiRes: any) => {
-	const [hourlyData, setHourlyData] = useState<HourlyData>(
+	const [hourlyData,setHourlyData] = useState<HourlyData>(
 		getHourlyData(apiRes)
 	);
 	useEffect(() => {
 		setHourlyData(getHourlyData(apiRes));
-	}, [apiRes]);
+	},[apiRes]);
 	const night = useNight();
 	return (
-		<div>
-			<h2 className="mb-4 text-3xl font-semibold md:text-5xl font-title text-neutral-900 dark:text-neutral-50">
-				Hourly Weather Forecast
-			</h2>
-			<div className="overflow-hidden h-36 md:h-56 pb-4">
-				<ul className="grid grid-flow-col gap-8 overflow-x-auto md:h-60 py-4 h-40">
-					{hourlyData.map((anHourData) => {
-						return (
-							<HourlyWeatherCard key={anHourData.time} {...anHourData} night={night} />
-						);
-					})}
-				</ul>
-			</div>
+		<div className="overflow-hidden h-36 md:h-56 pb-4">
+			<ul className="grid grid-flow-col gap-8 overflow-x-auto md:h-60 py-4 h-40">
+				{hourlyData.map((anHourData) => {
+					return (
+						<HourlyWeatherCard key={anHourData.time} {...anHourData} night={night} />
+					);
+				})}
+			</ul>
 		</div>
 	);
 };
--- src/components/DailyCarousel.tsx
+++ src/components/DailyCarousel.tsx
@@ -1,20 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState,useEffect } from "react";
 import getDailyData from "../helpers/getDailyData";
 import { DailyWeatherCard } from "./WeatherCard";
 
 const DailyCarousel = (apiRes: any) => {
-	const [dailyData, setDailyData] = useState(getDailyData(apiRes));
+	const [dailyData,setDailyData] = useState(getDailyData(apiRes));
 	useEffect(() => {
 		setDailyData(getDailyData(apiRes));
-	}, [apiRes]);
+	},[apiRes]);
 	return (
 		<div className="mt-4 md:mt-8">
-			<h2 className="mb-4 text-3xl font-semibold md:text-5xl font-title text-neutral-900 dark:text-neutral-50">
-				Daily Weather Forecast
-			</h2>
 			<div className="overflow-hidden h-36 md:h-56 pb-4">
 				<ul className="grid grid-flow-col gap-8 overflow-x-auto h-40 md:h-60 py-4">
-					{dailyData.map((daily, index) => (
+					{dailyData.map((daily,index) => (
 						<DailyWeatherCard key={index} {...daily} />
 					))}
 				</ul>
