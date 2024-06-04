2fdf72b6bed5a25ddc1a1916d7416272fb5642ec
bhendi-boi <bhendiisgreen@gmail.com>
2024-06-04 14:40:39 +0530

    minor design changes in `SunriseSunset`

--- src/components/SunriseSunset.tsx
+++ src/components/SunriseSunset.tsx
@@ -8,13 +8,13 @@ const SunriseSunset = ({ apiRes }: { apiRes: APIRES }) => {
         <div className="flex h-28 md:h-40 my-8 md:max-w-lg md:mx-auto gap-4 md:gap-10">
             <div className="flex-1 text-center bg-white rounded-lg">
                 <WiSunrise className="w-full h-2/3 md:h-4/5 text-amber-600" />
-                <p className="text-black font-title">
+                <p className="text-black font-title font-bold">
                     {getTime(apiRes["daily"]["sunrise"][0])}
                 </p>
             </div>
             <div className="flex-1 text-center bg-white rounded-lg">
                 <WiSunset className="w-full h-2/3 md:h-4/5 text-amber-400" />
-                <p className="text-black font-title">
+                <p className="text-black font-title font-bold">
                     {getTime(apiRes["daily"]["sunset"][0])}
                 </p>
             </div>
