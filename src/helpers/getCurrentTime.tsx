47920c0fe9f44724e1e43a20099ee88ca2056660
bhendi-boi <bhendiisgreen@gmail.com>
2024-06-04 14:12:20 +0530

    removed a bug in `getCurrentTime`

--- src/helpers/getCurrentTime.tsx
+++ src/helpers/getCurrentTime.tsx
@@ -29,7 +29,7 @@ function getCurrentTime(apiRes: any) {
 		", " +
 		currentHours +
 		":" +
-		(currentMin ? "00" : (currentMin)) +
+		(currentMin === 0 ? "00" : (currentMin)) +
 		" " +
 		TIMEZONE
 	);
